9de9f5aa1869d044128d506aefba3ca9c82e25ca
Nuzhat98 <nuzhat.islam@g.bracu.ac.bd>
2024-11-11 19:00:32 +0600

    Initial Commit

---
+++ src/test/java/com/example/CGPA_Calculator/CgpaCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CGPA_Calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgpaCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
